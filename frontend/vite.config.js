c8c935155ac607799bab09c4add1d3d86cb4b046
JoeAxe2027 <jab32@umsystem.edu>
2026-04-20 16:24:46 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://host.docker.internal:8000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
